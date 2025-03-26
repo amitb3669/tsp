3df9af5d1a6fee106b12fd88ebd1158e7e1ab8fa
amitb3669 <150140337+amitb3669@users.noreply.github.com>
2025-03-26 20:37:06 +0530

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/vnd.apple.mpegurl");
+
+$api_url = "https://maxplay-tv.fun/tvn.php"; // API URL
+
+$m3u8_url = file_get_contents($api_url);
+
+if ($m3u8_url) {
+    echo $m3u8_url;
+} else {
+    echo "Error fetching stream URL";
+}
+?>
