36e6265c719e9dfa7b0e9456f51e5c4ded96d315
amitb3669 <150140337+amitb3669@users.noreply.github.com>
2025-03-26 22:20:36 +0530

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/vnd.apple.mpegurl");
 
-$api_url = "https://maxplay-tv.fun/tvn.php"; // API URL
+$api_url = "https://maxplay-tv.fun/cl.php?id=f064e069-8861-43d5-ac79-828b029a50a1"; // API URL
 
 $m3u8_url = file_get_contents($api_url);
 
